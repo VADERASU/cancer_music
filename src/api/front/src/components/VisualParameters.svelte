1497799bbc6899175e2c3a0ef1cbc5a315669143
Rostyslav Hnatyshyn <rostyslav.hnatyshyn@gmail.com>
2023-06-20 13:17:00 -0700

    cleaned up vis parameters

--- src/api/front/src/components/VisualParameters.svelte
+++ src/api/front/src/components/VisualParameters.svelte
@@ -24,37 +24,58 @@
         values[selected][val] ? values[selected][val] : d;
 </script>
 
-{#each Object.keys(vis) as m}
-    <button on:click={() => (selected = m)}>{m}</button>
-{/each}
-<br />
-
-{#if selected}
-    {#key selected}
-        <span>{selected}</span>
-        <span>Color</span><input
-            type="color"
-            on:change={(e) => modifyValue(selected, "color", colorNotes, e)}
-            value={getValue("color", selected, "black")}
-        />
-        <span>Transparency</span><input
-            type="number"
-            step="0.1"
-            min="0"
-            max="1"
-            on:change={(e) =>
-                modifyValue(selected, "transparency", modifyAlpha, e)}
-            value={getValue("transparency", selected, 1)}
-        />
-        <span>Size</span><input
-            type="number"
-            step="1"
-            min="1"
-            on:change={(e) => modifyValue(selected, "size", modifySize, e)}
-            value={getValue("size", selected, 1)}
-        />
-    {/key}
-{/if}
+<div class="stack">
+    <div class="container">
+        {#each Object.keys(vis) as m}
+            <button on:click={() => (selected = m)}>{m}</button>
+        {/each}
+    </div>
+    {#if selected}
+        {#key selected}
+            <span>{selected}</span>
+            <div class="container">
+                <label for="color">Color</label><input
+                    type="color"
+                    name="color"
+                    on:change={(e) =>
+                        modifyValue(selected, "color", colorNotes, e)}
+                    value={getValue("color", selected, "black")}
+                />
+            </div>
+            <div class="container">
+                <label for="trans">Transparency</label><input
+                    type="number"
+                    step="0.1"
+                    name="trans"
+                    min="0"
+                    max="1"
+                    on:change={(e) =>
+                        modifyValue(selected, "transparency", modifyAlpha, e)}
+                    value={getValue("transparency", selected, 1)}
+                />
+            </div>
+            <div class="container">
+                <label for="size">Size</label><input
+                    type="number"
+                    name="size"
+                    step="1"
+                    min="1"
+                    on:change={(e) =>
+                        modifyValue(selected, "size", modifySize, e)}
+                    value={getValue("size", selected, 1)}
+                />
+            </div>
+        {/key}
+    {/if}
+</div>
 
 <style>
+    .stack {
+        flex-direction: column;
+        display: flex;
+    }
+    .container {
+        display: flex;
+        justify-content: space-between;
+    }
 </style>
